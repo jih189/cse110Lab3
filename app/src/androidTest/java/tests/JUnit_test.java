7ed8a9cc163a4418df26b6abca7528a19d3715c4
jih189 <jih189@ucsd.edu>
2016-04-13 10:47:46 -0500

    CSE 110 Lab3

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,28 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.f.testproject.MainActivity;
+import com.example.f.testproject.R;
+/**
+ * Created by f on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public JUnit_test(){
+
+        super(MainActivity.class);
+    }
+
+    public void test_first()
+    {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye world!", tester);
+    }
+}
